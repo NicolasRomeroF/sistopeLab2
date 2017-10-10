ad46f0d55405344df49995051b30b9058f05d197
Nicolas <nicolas.romero.f@usach.cl>
2017-10-09 22:58:52 -0300

    creado main y recibir parametros

---
+++ main.c
@@ -0,0 +1,154 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+struct structHebra
+{
+	pthread_t id;
+	int x;
+	int y;
+	char** palabras;
+	pthread_mutex_t mutex;
+} typedef Hebra;
+
+
+void printMatriz(char** matriz, int N, int M)
+{
+	int i, j;
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < M; j++)
+		{
+			printf("%c ", matriz[i][j]);
+		}
+		printf("\n");
+	}
+	printf("Fin print\n");
+}
+
+char** crearMatriz(int N, int M)
+{
+	char** matriz = (char**)malloc(sizeof(char*)*N);
+	int i, j;
+	if (matriz == NULL)
+	{
+		printf("ERROR: No se pudo asignar memoria a la matriz\n");
+		return NULL;
+	}
+	for (i = 0; i < M; i++)
+	{
+		matriz[i] = (char*)malloc(sizeof(char) * M);
+		if (matriz[i] == NULL)
+		{
+			printf("ERROR: No se pudo asignar memoria a la matriz\n");
+			return NULL;
+		}
+	}
+	for (i = 0;i < N; i++)
+	{
+		for (j = 0; j < M; j++)
+		{
+			matriz[i][j] = 'c';
+		}
+	}
+	return matriz;
+}
+
+void* ubicar (void* id)
+{
+
+}
+
+
+
+int main(int argc, char **argv)
+{
+	//Aqui declaramos las variables a recibir por medio de optarg
+	char* iName;
+	int hCant = 0;
+	int cCant = 0;
+	int nMatriz;
+	int mMatriz;
+	char* sName;
+	int dFlag = 0;
+	int index;
+	int c;
+
+	opterr = 0;
+
+	//Con este ciclo se obtienen los argumentos ingresados por consola y se asignan
+	//a las variables anteriormente declaradas
+	while ((c = getopt (argc, argv, "i:h:c:n:m:s:d")) != -1) {
+		switch (c) {
+		case 'i':
+			iName = optarg;
+			break;
+		case 'h':
+			sscanf(optarg, "%d", &hCant);
+			break;
+		case 'c':
+			sscanf(optarg, "%d", &cCant);
+			break;
+		case 'n':
+			sscanf(optarg, "%d", &nMatriz);
+			break;
+		case 'm':
+			sscanf(optarg, "%d", &mMatriz);
+			break;
+		case 's':
+			sName = optarg;
+			break;
+		case 'd':
+			dFlag = 1;
+			break;
+
+		//Caso en que se ingrese una opcion desconocida
+		case '?':
+			if (optopt == 'i' || optopt == 'h' || optopt == 'c' || optopt == 'n' || optopt == 'm' || optopt == 's') {
+				fprintf (stderr, "Opcion -%c requiere un argumento.\n", optopt);
+			}
+			else if (isprint (optopt)) {
+				fprintf (stderr, "Opcion desconocida `-%c'.\n", optopt);
+			}
+			else {
+				fprintf (stderr, "Caracter desconocido `\\x%x'.\n", optopt);
+
+			}
+			return 1;
+		default:
+			abort ();
+		}
+
+	}
+
+	//Se muestran por pantalla los datos ingresados.
+	printf ("iName = %s, hCant = %d, cCant = %d, nMatriz = %d, mMatriz = %d, sName = %s, dFlag = %d\n", iName, hCant, cCant, nMatriz, mMatriz, sName, dFlag);
+
+	//Se muestra mensaje en caso de ingresar un argumento que no existe
+	for (index = optind; index < argc; index++) {
+		printf ("Argumento no existente %s\n", argv[index]);
+	}
+
+	char** matriz = crearMatriz(nMatriz, mMatriz);
+	if (matriz == NULL)
+	{
+		return -1;
+	}
+
+	printf("Matriz N: %d M: %d\n", nMatriz, mMatriz);
+	printMatriz(matriz, nMatriz, mMatriz);
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
